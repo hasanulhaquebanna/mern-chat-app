18e56cc2f34e09da74543540a98277abf05ed5cd
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-06-21 06:02:07 +0600

    axios install for fetching api

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
+import { Button } from "@chakra-ui/react";
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  );
 };
 
 export default App;
