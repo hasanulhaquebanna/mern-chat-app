67454ba25843bf43031f7262dac4dbe2f0ef3001
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-06-21 17:59:55 +0600

    login page done

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,23 +5,51 @@ const Login = ({ styles }) => {
   const googleAuth = () => {};
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>Log in Form</h1>
-      <div className={styles.form_container}>
-        <div className={styles.left}>
-          <img className={styles.img} src="./images/login.jpg" alt="login" />
+      <h1 className="text-[40px] font-semibold text-[#2c444e] relative flex items-center justify--center after:content-[''] after:w-[400px] after:h-1 after:rounded-[1px] after:-bottom-5 after:bg-[#2c444e] after:absolute">
+        Log in Form
+      </h1>
+      <div className="flex  mt-[45px] w-[800px] h-[450px] bg-white shadow-formContainer">
+        <div className="flex-[1.5] overflow-hidden relative rounded-tl-[50px] rounded-bl-[50px]">
+          <img
+            className="w-[160%] absolute -left-[150px] -top-[50px]"
+            src="./images/login.jpg"
+            alt="login"
+          />
         </div>
-        <div className={styles.right}>
-          <h2 className={styles.from_heading}>Members Log in</h2>
-          <input type="text" className={styles.input} placeholder="Email" />
-          <input type="text" className={styles.input} placeholder="Password" />
-          <button className={styles.btn}>Log In</button>
-          <p className={styles.text}>or</p>
-          <button className={styles.google_btn} onClick={googleAuth}>
-            <img src="./images/google.png" alt="google icon" />
-            <span>Sing in with Google</span>
+        <div className="flex-2 flex flex-col items-center justify-center">
+          <h2 className="text-[25px] font-normal text-[#2c444e] mb-[30px]">
+            Members Log in
+          </h2>
+          <input
+            type="text"
+            className="w-[320px] h-[35px] p-[5px] my-[5px] mx-0 outline-none border border-[#2c444e] rounded-[2px] text-[13px]"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            className="w-[320px] h-[35px] p-[5px] my-[5px] mx-0 outline-none border border-[#2c444e] rounded-[2px] text-[13px]"
+            placeholder="Password"
+          />
+          <button className="text-lg font-medium py-3 px-[25px] text-white bg-[#ffc801] rounded-[12px] mt-[10px] mr-0 mb-0 ml-0 outline-none border-none cursor-pointer">
+            Log In
           </button>
-          <p className={styles.text}>
-            New Here ? <Link to="/signup">Sing Up</Link>
+          <p className="text-sm text-[#2c444e] m-[5px] mx-0 p-0">or</p>
+          <button
+            className="w-[230px] h-10 rounded-[5px] border-none outline-none bg-white shadow-googleBtn text-base font-medium mt-0 mr-0 mb-5 ml-0 text-[#2c444e] pointer flex items-center justify-center relative"
+            onClick={googleAuth}
+          >
+            <img
+              src="./images/google.png"
+              className="w-[30px] h-[30px] object-cover"
+              alt="google icon"
+            />
+            <span className="ml-[10px]">Sing in with Google</span>
+          </button>
+          <p className="text-sm text-[#2c444e] m-[5px] mx-0 p-0">
+            New Here ?{" "}
+            <Link to="/signup" className="text-base font-medium text-[#ffc801]">
+              Sing Up
+            </Link>
           </p>
         </div>
       </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
+      boxShadow: {
+        formContainer: "3px 4px 36px -6px rgba(0,0,0,0.4)",
+        googleBtn:
+          "rgb(0 0 0 / 20%) 0px 3px 1px -2px, rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px",
+      },
     },
   },
   plugins: [],
