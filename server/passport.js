38c1dddd61849e9b0b1c86776d8febd28ab2063c
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-06-21 07:10:29 +0600

    passport bug fixed

--- server/passport.js
+++ server/passport.js
@@ -2,16 +2,18 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const passport = require("passport");
 const { CLIENTID, CLIENTSECRET, CLIENTURL } = require("./env");
 
-app.use(
-  new GoogleStrategy({
-    clientID: CLIENTID,
-    clientSecret: CLIENTSECRET,
-    callbackURL: "/auth/google/callback",
-    scope: ["profile", "email"],
-  }),
-  function (accesToken, refreshToken, profile, callback) {
-    callback(null, profile);
-  }
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: CLIENTID,
+      clientSecret: CLIENTSECRET,
+      callbackURL: "/auth/google/callback",
+      scope: ["profile", "email"],
+    },
+    function (accesToken, refreshToken, profile, callback) {
+      callback(null, profile);
+    }
+  )
 );
 
 passport.serializeUser((user, done) => {
