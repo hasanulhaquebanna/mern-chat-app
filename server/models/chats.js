34e925ee1be75eca798c0addfdb439a0e935de21
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-06-21 06:11:26 +0600

    chat model done

---
+++ server/models/chats.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const chatModel = mongoose.Schema(
+  {
+    chatName: {
+      type: String,
+      trim: true,
+    },
+    isGroup: {
+      type: Boolean,
+      default: false,
+    },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timeStamps: true }
+);
+
+module.exports = mongoose.model("Chat", chatModel);
