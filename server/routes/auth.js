576f362926159430433a11d5dc70a6c820b917b0
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-06-22 06:43:41 +0600

    emoji picker, express asyn handler and server configured

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,13 +2,14 @@ const router = require("express").Router();
 const passport = require("passport");
 
 const googleCallBack = require("../controllers/auth/googleCallBack");
-const loginFailed = require("../controllers/auth/loginFailed");
+const googleLoginFailed = require("../controllers/auth/googleLoginFailed");
+const googleLoginSuccess = require("../controllers/auth/googleLoginSuccess");
 const { CLIENTURL } = require("../env");
 
 // passprort with oauth20
-router.get("/auth/login/success");
+router.get("/auth/login/success", googleLoginSuccess);
 router.get("/auth/google/callback", googleCallBack);
-router.get("/auth/login/failed", loginFailed);
+router.get("/auth/login/failed", googleLoginFailed);
 router.get(
   "/auth/google",
   passport.authenticate("google", ["profile", "email"])
@@ -17,5 +18,7 @@ router.get("/auth/logout", (req, res) => {
   req.logOut();
   res.redirect(CLIENTURL);
 });
+// rest api routes
+router.post("/auth/user/login", authUserLogin);
 
 module.exports = router;
