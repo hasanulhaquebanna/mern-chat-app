f8ce48dfeab67e303d4c429189d2c4d2bd6b6546
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-06-24 08:13:27 +0600

    home page fixed bug

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 const Home = () => {
+  const history = useHistory();
+  //
+  useEffect(() => {
+    if (!localStorage.getItem("userinfo")) {
+      history.push("/login");
+    }
+  }, [history]);
   return <div>Home</div>;
 };
 
