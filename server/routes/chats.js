e5e0cf3edfc997f3d2080ca283ca57409f3d8f08
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-06-25 07:22:45 +0600

    remove from group added

--- server/routes/chats.js
+++ server/routes/chats.js
@@ -6,13 +6,14 @@ const createChat = require("../controllers/chats/createChat");
 const getAllChats = require("../controllers/chats/getAllChats");
 const createGroup = require("../controllers/chats/createGroup");
 const renameGroup = require("../controllers/chats/renameGroup");
+const addToGroup = require("../controllers/chats/addToGroup");
+const removeFromGroup = require("../controllers/chats/removeFromGroup");
 
 router.post("/chats", userAuthorization, createChat);
 router.get("/chats", userAuthorization, getAllChats);
 router.post("/group/creategroup", userAuthorization, createGroup);
-// router.post('/group/getgroups',getGroups)
-// router.post('/group/remove',removeGroup)
 router.put("/group/renamegroup", userAuthorization, renameGroup);
-// router.post('/group/add',addingGroup)
+router.put("/group/addtogroup", userAuthorization, addToGroup);
+router.post("/group/removefromgroup", userAuthorization, removeFromGroup);
 
 module.exports = router;
