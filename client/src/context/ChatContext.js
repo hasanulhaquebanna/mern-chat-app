b01529a3b16d36dbc62d1e75b7eefb163f420afc
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-06-25 15:19:27 +0600

    fetching every user from localstorage setup completed

--- client/src/context/ChatContext.js
+++ client/src/context/ChatContext.js
@@ -1,9 +1,28 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const ChatContext = createContext();
 
 const ChatProvider = ({ children }) => {
-  return <ChatContext.Provider>{children}</ChatContext.Provider>;
+  const history = useHistory();
+  const [user, setUser] = useState();
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    if (!userInfo) {
+      history.push("/login");
+    } else {
+      setUser(userInfo);
+    }
+  }, [history]);
+  return (
+    <ChatContext.Provider value={{ user, setUser }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const ChatState = () => {
+  return useContext(ChatContext);
 };
 
 export default ChatProvider;
