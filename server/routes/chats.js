d4f73fe2228ed1e52a02732bb2974a392b1bd1f6
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-06-25 06:33:23 +0600

    creating and getting chat done

--- server/routes/chats.js
+++ server/routes/chats.js
@@ -3,9 +3,10 @@ const passport = require("passport");
 
 const userAuthorization = require("../middleware/userAuthorization");
 const createChat = require("../controllers/chats/createChat");
+const getAllChats = require("../controllers/chats/getAllChats");
 
 router.post("/chats", userAuthorization, createChat);
-// router.post('/chats',getChat)
+router.get("/chats", userAuthorization, getAllChats);
 // router.post('/group/creategroup',createGroup)
 // router.post('/group/getgroups',getGroups)
 // router.post('/group/remove',removeGroup)
